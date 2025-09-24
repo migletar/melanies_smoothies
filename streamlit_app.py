1c2f44de2991bc20525def50c5484ed4bb57fd71
migletar <migle.tareseviciute@twoday.com>
2025-09-24 17:58:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 # Import python packages
 import streamlit as st
-import pandas as pd
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import requests
 
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
@@ -14,17 +13,9 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie')
 st.write("The name of your smoothie will be", name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#st.dataframe(data=my_dataframe, width='stretch)
-#st.stop()
-
-# convert the snowpark datagrame to pandas dataframe so we can use the LOC function
-pd_df=my_dataframe.to_pandas()
-# st.dataframe(pd_df)
-# st.stop()
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
         'Choose up to 5 ingredients:'
@@ -33,17 +24,11 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
+
     ingredients_string=''
 
     for fruit_chosen in ingredients_list:
-              ingredients_string += fruit_chosen + ' '
-
-              search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-              st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-      
-              st.subheader(fruit_chosen + 'Nutrition Information')
-              fruityvice_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
-              pd_df = st.dataframe(data=fruityvice_response.json(), width='stretch')
+        ingredients_string += fruit_chosen + ' '
 
     #st.write(ingredients_string)
 
@@ -61,7 +46,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-
-
 
 
